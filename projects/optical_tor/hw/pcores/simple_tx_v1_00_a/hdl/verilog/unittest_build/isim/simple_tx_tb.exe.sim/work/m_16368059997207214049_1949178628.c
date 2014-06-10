5dd838e3fc0568accdbedcbcb795e2b70b9bccb4
manihatn <nm525@cl.cam.ac.uk>
2014-06-10 19:26:56 +0100

    updated new pcore

---
+++ projects/optical_tor/hw/pcores/simple_tx_v1_00_a/hdl/verilog/unittest_build/isim/simple_tx_tb.exe.sim/work/m_16368059997207214049_1949178628.c
@@ -0,0 +1,607 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x8ddf5b5d */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "/local/scratch-1/nm525/workbench/ppon/wb/tor/projects/optical_tor/hw/pcores/simple_tx_v1_00_a/hdl/verilog/simple_tx_tb.v";
+static unsigned int ng1[] = {0U, 0U};
+static int ng2[] = {4, 0};
+static unsigned int ng3[] = {1U, 0U};
+
+
+
+static void Initial_12_0(char *t0)
+{
+    char t11[8];
+    char *t1;
+    char *t2;
+    char *t3;
+    unsigned int t4;
+    unsigned int t5;
+    unsigned int t6;
+    int t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t12;
+    char *t13;
+    char *t14;
+    char *t15;
+    unsigned int t16;
+    unsigned int t17;
+    unsigned int t18;
+    unsigned int t19;
+    unsigned int t20;
+    char *t21;
+    char *t22;
+    char *t23;
+    unsigned int t24;
+    unsigned int t25;
+    unsigned int t26;
+    unsigned int t27;
+    unsigned int t28;
+    unsigned int t29;
+    unsigned int t30;
+    unsigned int t31;
+    char *t32;
+
+LAB0:    t1 = (t0 + 4600U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(12, ng0);
+
+LAB4:    xsi_set_current_line(13, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 3528);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(14, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 3688);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(15, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t2 + 4);
+    t4 = *((unsigned int *)t3);
+    t5 = (~(t4));
+    t6 = *((unsigned int *)t2);
+    t7 = (t6 & t5);
+    t8 = (t0 + 10048);
+    *((int *)t8) = t7;
+
+LAB5:    t9 = (t0 + 10048);
+    if (*((int *)t9) > 0)
+        goto LAB6;
+
+LAB7:    xsi_set_current_line(16, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 3688);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(17, ng0);
+
+LAB15:    xsi_set_current_line(17, ng0);
+    t2 = (t0 + 4408);
+    xsi_process_wait(t2, 10000LL);
+    *((char **)t1) = &&LAB16;
+
+LAB1:    return;
+LAB6:    xsi_set_current_line(15, ng0);
+    t10 = (t0 + 4408);
+    xsi_process_wait(t10, 10000LL);
+    *((char **)t1) = &&LAB8;
+    goto LAB1;
+
+LAB8:    xsi_set_current_line(15, ng0);
+    t12 = (t0 + 3528);
+    t13 = (t12 + 56U);
+    t14 = *((char **)t13);
+    memset(t11, 0, 8);
+    t15 = (t14 + 4);
+    t16 = *((unsigned int *)t15);
+    t17 = (~(t16));
+    t18 = *((unsigned int *)t14);
+    t19 = (t18 & t17);
+    t20 = (t19 & 1U);
+    if (t20 != 0)
+        goto LAB12;
+
+LAB10:    if (*((unsigned int *)t15) == 0)
+        goto LAB9;
+
+LAB11:    t21 = (t11 + 4);
+    *((unsigned int *)t11) = 1;
+    *((unsigned int *)t21) = 1;
+
+LAB12:    t22 = (t11 + 4);
+    t23 = (t14 + 4);
+    t24 = *((unsigned int *)t14);
+    t25 = (~(t24));
+    *((unsigned int *)t11) = t25;
+    *((unsigned int *)t22) = 0;
+    if (*((unsigned int *)t23) != 0)
+        goto LAB14;
+
+LAB13:    t30 = *((unsigned int *)t11);
+    *((unsigned int *)t11) = (t30 & 1U);
+    t31 = *((unsigned int *)t22);
+    *((unsigned int *)t22) = (t31 & 1U);
+    t32 = (t0 + 3528);
+    xsi_vlogvar_assign_value(t32, t11, 0, 0, 1);
+    t2 = (t0 + 10048);
+    t7 = *((int *)t2);
+    *((int *)t2) = (t7 - 1);
+    goto LAB5;
+
+LAB9:    *((unsigned int *)t11) = 1;
+    goto LAB12;
+
+LAB14:    t26 = *((unsigned int *)t11);
+    t27 = *((unsigned int *)t23);
+    *((unsigned int *)t11) = (t26 | t27);
+    t28 = *((unsigned int *)t22);
+    t29 = *((unsigned int *)t23);
+    *((unsigned int *)t22) = (t28 | t29);
+    goto LAB13;
+
+LAB16:    xsi_set_current_line(17, ng0);
+    t3 = (t0 + 3528);
+    t8 = (t3 + 56U);
+    t9 = *((char **)t8);
+    memset(t11, 0, 8);
+    t10 = (t9 + 4);
+    t4 = *((unsigned int *)t10);
+    t5 = (~(t4));
+    t6 = *((unsigned int *)t9);
+    t16 = (t6 & t5);
+    t17 = (t16 & 1U);
+    if (t17 != 0)
+        goto LAB20;
+
+LAB18:    if (*((unsigned int *)t10) == 0)
+        goto LAB17;
+
+LAB19:    t12 = (t11 + 4);
+    *((unsigned int *)t11) = 1;
+    *((unsigned int *)t12) = 1;
+
+LAB20:    t13 = (t11 + 4);
+    t14 = (t9 + 4);
+    t18 = *((unsigned int *)t9);
+    t19 = (~(t18));
+    *((unsigned int *)t11) = t19;
+    *((unsigned int *)t13) = 0;
+    if (*((unsigned int *)t14) != 0)
+        goto LAB22;
+
+LAB21:    t27 = *((unsigned int *)t11);
+    *((unsigned int *)t11) = (t27 & 1U);
+    t28 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t28 & 1U);
+    t15 = (t0 + 3528);
+    xsi_vlogvar_assign_value(t15, t11, 0, 0, 1);
+    goto LAB15;
+
+LAB17:    *((unsigned int *)t11) = 1;
+    goto LAB20;
+
+LAB22:    t20 = *((unsigned int *)t11);
+    t24 = *((unsigned int *)t14);
+    *((unsigned int *)t11) = (t20 | t24);
+    t25 = *((unsigned int *)t13);
+    t26 = *((unsigned int *)t14);
+    *((unsigned int *)t13) = (t25 | t26);
+    goto LAB21;
+
+LAB23:    goto LAB1;
+
+}
+
+static void implSig1_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+
+LAB0:    t1 = (t0 + 4848U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng1)));
+    t3 = (t0 + 7216);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memcpy(t7, t2, 8);
+    xsi_driver_vfirst_trans(t3, 0, 31);
+
+LAB1:    return;
+}
+
+static void implSig2_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+
+LAB0:    t1 = (t0 + 5096U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng3)));
+    t3 = (t0 + 7280);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t2 + 4);
+    t11 = *((unsigned int *)t2);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t3, 0, 0);
+
+LAB1:    return;
+}
+
+static void implSig3_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+
+LAB0:    t1 = (t0 + 5344U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng1)));
+    t3 = (t0 + 7344);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memcpy(t7, t2, 8);
+    xsi_driver_vfirst_trans(t3, 0, 31);
+
+LAB1:    return;
+}
+
+static void implSig4_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+
+LAB0:    t1 = (t0 + 5592U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng1)));
+    t3 = (t0 + 7408);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 15U;
+    t9 = t8;
+    t10 = (t2 + 4);
+    t11 = *((unsigned int *)t2);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t3, 0, 3);
+
+LAB1:    return;
+}
+
+static void implSig5_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+
+LAB0:    t1 = (t0 + 5840U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng3)));
+    t3 = (t0 + 7472);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t2 + 4);
+    t11 = *((unsigned int *)t2);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t3, 0, 0);
+
+LAB1:    return;
+}
+
+static void implSig6_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+
+LAB0:    t1 = (t0 + 6088U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng3)));
+    t3 = (t0 + 7536);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t2 + 4);
+    t11 = *((unsigned int *)t2);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t3, 0, 0);
+
+LAB1:    return;
+}
+
+static void implSig7_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+
+LAB0:    t1 = (t0 + 6336U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng1)));
+    t3 = (t0 + 7600);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memcpy(t7, t2, 8);
+    xsi_driver_vfirst_trans(t3, 0, 31);
+
+LAB1:    return;
+}
+
+static void implSig8_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+
+LAB0:    t1 = (t0 + 6584U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng3)));
+    t3 = (t0 + 7664);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t2 + 4);
+    t11 = *((unsigned int *)t2);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t3, 0, 0);
+
+LAB1:    return;
+}
+
+static void implSig9_execute(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+
+LAB0:    t1 = (t0 + 6832U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    t2 = ((char*)((ng3)));
+    t3 = (t0 + 7728);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t2 + 4);
+    t11 = *((unsigned int *)t2);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t3, 0, 0);
+
+LAB1:    return;
+}
+
+
+extern void work_m_16368059997207214049_1949178628_init()
+{
+	static char *pe[] = {(void *)Initial_12_0,(void *)implSig1_execute,(void *)implSig2_execute,(void *)implSig3_execute,(void *)implSig4_execute,(void *)implSig5_execute,(void *)implSig6_execute,(void *)implSig7_execute,(void *)implSig8_execute,(void *)implSig9_execute};
+	xsi_register_didat("work_m_16368059997207214049_1949178628", "isim/simple_tx_tb.exe.sim/work/m_16368059997207214049_1949178628.didat");
+	xsi_register_executes(pe);
+}
---
+++ projects/optical_tor/hw/pcores/simple_tx_v1_00_a/hdl/verilog/unittest_build/isim/simple_tx_tb.exe.sim/work/simple_tx_tb.exe_main.c
@@ -0,0 +1,69 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+#include "xsi.h"
+
+struct XSI_INFO xsi_info;
+
+char *PROC_COMMON_V3_00_A_P_2444876401;
+char *IEEE_P_2592010699;
+char *STD_STANDARD;
+char *IEEE_P_3499444699;
+char *IEEE_P_3620187407;
+char *PROC_COMMON_V3_00_A_P_1541446978;
+char *PROC_COMMON_V3_00_A_P_4147123038;
+char *IEEE_P_1242562249;
+char *IEEE_P_0017514958;
+char *SYNOPSYS_P_3308480207;
+
+
+int main(int argc, char **argv)
+{
+    xsi_init_design(argc, argv);
+    xsi_register_info(&xsi_info);
+
+    xsi_register_min_prec_unit(-12);
+    work_m_03609373904439264904_0342672298_init();
+    work_m_04348368895518202626_1765630771_init();
+    work_m_16368059997207214049_1949178628_init();
+    ieee_p_2592010699_init();
+    ieee_p_3499444699_init();
+    ieee_p_3620187407_init();
+    proc_common_v3_00_a_p_2444876401_init();
+    proc_common_v3_00_a_p_1541446978_init();
+    ieee_p_1242562249_init();
+    synopsys_p_3308480207_init();
+    ieee_p_0017514958_init();
+    proc_common_v3_00_a_p_4147123038_init();
+    axi_lite_ipif_v1_01_a_a_3744965942_3306564128_init();
+    axi_lite_ipif_v1_01_a_a_1097472162_3306564128_init();
+    axi_lite_ipif_v1_01_a_a_0950711865_3306564128_init();
+    work_a_3283278744_3306564128_init();
+
+
+    xsi_register_tops("work_m_16368059997207214049_1949178628");
+
+    PROC_COMMON_V3_00_A_P_2444876401 = xsi_get_engine_memory("proc_common_v3_00_a_p_2444876401");
+    IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
+    xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
+    STD_STANDARD = xsi_get_engine_memory("std_standard");
+    IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
+    IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
+    PROC_COMMON_V3_00_A_P_1541446978 = xsi_get_engine_memory("proc_common_v3_00_a_p_1541446978");
+    PROC_COMMON_V3_00_A_P_4147123038 = xsi_get_engine_memory("proc_common_v3_00_a_p_4147123038");
+    IEEE_P_1242562249 = xsi_get_engine_memory("ieee_p_1242562249");
+    IEEE_P_0017514958 = xsi_get_engine_memory("ieee_p_0017514958");
+    SYNOPSYS_P_3308480207 = xsi_get_engine_memory("synopsys_p_3308480207");
+
+    return xsi_run_simulation(argc, argv);
+
+}
